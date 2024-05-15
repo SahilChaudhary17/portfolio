443fd7e46dbc95be1928ee4109f4f0acb30ac768
Sahil Chaudhary <sahilch38@gmail.com>
2024-05-15 21:49:52 +0530

    updates v1

---
+++ src/lib/content/about.ts
@@ -0,0 +1,21 @@
+import { AboutSectionType } from '@/lib/types/sections'
+
+export const aboutSection: AboutSectionType = {
+    title: 'about me',
+    // Paragraphs need to be changed from containers/About.tsx
+    // Because it wasn't possible to insert anchor tags like this
+    list: {
+    title: 'Here are a few technologies I’ve been working with recently:',
+    items: [
+        'ReactJs',
+        'NextJs',
+        'TailwindCSS',
+        'Figma',
+        'Canva',
+        'Vercel',
+        'AWS',
+        'Google Cloud',
+    ],
+    },
+    img: '/sahilprofile.jpg',
+};
---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,40 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/sahil.chaudhary.123276',
+  instagram: 'https://www.instagram.com/sahil._.1012',
+  twitter: 'https://twitter.com/sahil_1012',
+  github: 'https://github.com/SahilChaudhary17',
+  linkedin: 'https://www.linkedin.com/in/sahilchaudhary17',
+  leetcode: 'https://www.leetcode.com/in/sahilchaudhary17'
+};
+
+export const author = {
+  name: 'Sahil Chaudhary',
+  email: 'sahil.techwork@gmail.com',
+};
+
+export const seoData = {
+  title: 'Sahil Chaudhary | Front-End Developer',
+  description:
+    'Sahil Chaudhary is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://sahilchaudhary.vercel.app/',
+  keywords: [
+    'Sahil',
+    'Sahil Chaudhary',
+    '@sahilchaudhary',
+    'sahilchaudhary',
+    'Portfolio',
+    'Sahil Portfolio ',
+    'Sahil Chaudhary Portfolio',
+  ],
+};
