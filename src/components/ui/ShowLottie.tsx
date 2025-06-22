3be6c62d5d57b68b7efbdab32efdb7d52f602c35
Sahil Chaudhary <sahilch38@gmail.com>
2025-06-22 22:58:00 +0530

    projects

--- src/components/ui/ShowLottie.tsx
+++ src/components/ui/ShowLottie.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Player } from '@lottiefiles/react-lottie-player';
+import { useEffect, useState } from 'react';
 
 type Props = {
   path: any;
@@ -7,9 +8,15 @@ type Props = {
 };
 
 const ShowLottie = ({ path, className = '' }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <div className={`max-w-sm md:max-w-md ${className}`}>
-      <Player autoplay loop src={path}></Player>
+      {isMounted && <Player autoplay loop src={path}></Player>}
     </div>
   );
 };
